d97564fdb38909b46413656f6535ec74b9b334e4
Jordan Hall <jordan@hall05.co.uk>
2020-03-28 00:06:05 +0000

    Add initial trait with stub methods

---
+++ src/Traits/HasAttributeValuePrediction.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DivineOmega\EloquentAttributeValuePrediction\Traits;
+
+class HasAttributeValuePrediction
+{
+    public function predict()
+    {
+
+    }
+
+    public function getPredictions(): array
+    {
+
+    }
+}
